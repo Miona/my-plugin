edaa5a7aeb4afef04266da2d27cc9954f698de70
Miona <shruti.taldar@gmail.com>
2014-08-18 15:39:53 +0530

    final commit

--- recents.php
+++ recents.php
@@ -32,12 +32,13 @@ class Recent_Comments extends WP_Widget {
 	function widget( $args, $instance ) {
 		global $comments, $comment;
 
-		
-                
+		echo '<div class="comments">';
+                 
  		extract($args, EXTR_SKIP);
  		$output = '';
                 
 		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Comments' );
+                
 		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
 		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
 		if ( ! $number )
@@ -49,36 +50,51 @@ class Recent_Comments extends WP_Widget {
 			$output .= $before_title . $title . $after_title;
 
 		$output .= '<ul id="recentcomments">';
+               
                 
 		if ( $comments ) {
 			// Prime cache for associated posts. (Prime post term cache if we need it for permalinks.)
 			$post_ids = array_unique( wp_list_pluck( $comments, 'comment_post_ID' ) );
 			_prime_post_caches( $post_ids, strpos( get_option( 'permalink_structure' ), '%category%' ), false );
+                       
 
 			foreach ( (array) $comments as $comment) {
-                            if($instance['checkbox_2']){
+                            /*if($instance['checkbox_2']){
                                
-                            $com=get_comment_text();}
+                            $com=get_comment_text();}*/
                              if($instance['checkbox_1']){
+                               
                                 // echo "on";
-                             $posttitle=get_the_title($comment->comment_post_ID);}
-                            
+                             $posttitle=get_the_title($comment->comment_post_ID);
+                             }
+                           echo '<div class="combine">';
+                             
+                             if($instance['checkbox_2']){
+                               echo '<div class="text">';
+                            $com=get_comment_text();
+                            echo '</div>';
+                             }
+                             
                              if($instance['checkbox_3']){
+                                 
                                  $id = get_comment( get_comment_ID() )->user_id;
                                  $image=get_avatar($id,'32');
+                                  
                              }
-                            $output .=  '<li class="recentcomments">' . /*translators: comments widget: 1: comment author, 2: post link*/  sprintf(_x(' %1$s on %2$s <br>  %3$s <br> <br> %4$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . $posttitle . '</a>',$com,$image) . '</li>';
+                            
+                           echo "</div>";
+                            $output .=  '<li class="recentcomments">' . /*translators: comments widget: 1: comment author, 2: post link*/  sprintf(_x('<br><br> %1$s on %2$s <br>  %3$s %4$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . $posttitle . '</a>' ,'<div class="combine"><div class="avatar">'.$image .'</div>','<div class="text">'.$com.'</div></div>') . '</li>';
                                 //$output .=  '<li class="recentcomments">' . /*translators: comments widget: 1: comment author, 2: post link*/  sprintf(_x('%1$s on %2$s <br> %3$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>',get_comment_text()) . '</li>';
 			}
  		}
-		$output .= '</ul>';
+		//$output .= '</ul>';
 		$output .= $after_widget;
 
 		echo $output;
 		$cache[$args['widget_id']] = $output;
 		wp_cache_set('widget_recent_comments', $cache, 'widget');
 	}
-
+        
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
